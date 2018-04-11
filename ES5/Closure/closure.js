a95b50a89aa5bdd35aeaccedb075bb44a27eaa57
Nitin Sharma <nitin_sharma-ggn@external.mckinsey.com>
2018-04-11 11:25:32 +0530

    Added more on clousers

--- ES5/Closure/closure.js
+++ ES5/Closure/closure.js
@@ -1,4 +1,23 @@
+// Closure is when a function is able to remember and access its lexical scope even when that function is executing outside its lexical scope.
 
+// Closures are exposed nested functions
+
+/*
+
+If you declare a variable inside a function it won’t be available outside of it. But we can also define functions inside of other functions which will 
+have a scope on their own inside of them. Now the special thing about those nested functions is that they can access their parent’s variables.
+
+Every function defined in the global scope can access the variables in it. Those nested functions that we mentioned can access their parent function’s scope 
+but they themselves can’t be called from outside of the parent function. Unless we expose them somehow.
+
+We can expose that inner function so it can be used in the global scope. Now we can call it and use it as we wish. 
+But this exposed inner function actually references one of the variables in it’s parent’s scope. 
+Will that be a problem? No, of course it won’t because it’s a Closure!
+
+Scopes plays vital role in Clousers , functions are use for creating the new scopes and once excuted its scopes can persist if the nested function inside it is exposed 
+and refernces a outer scope variable.
+
+*/
 
 // When you pass an argument to a function it becomes available as a local variable.
 // More than one reference to a variable within function will persist the variable , even when the function has returned.
@@ -29,8 +48,7 @@ n() //Output : "b"
 //---------Closure #3:--------------
 /*
 When you pass an argument to a function it becomes available as a local variable.
-You can create a function that returns another function, which in turn returns its
-parent's argument
+You can create a function that returns another function, which in turn returns its parent's argument
 */
 
 function f(arg) {
@@ -45,16 +63,15 @@ var m = f(123);
 m();    // Output: 124
 
 /*
-Notice how arg++ was incremented after the function was defined and yet, when
-called, m() returned the updated value. This demonstrates how the function binds to
-its scope, not to the current variables and their values found in the scope.
+Notice how arg++ was incremented after the function was defined and yet, when called, m() returned the updated value. This demonstrates how the function binds to
+its scope, not to the current variables and their values found in the scope. 
 */
 
 function f() {
  var a = [];
  var i;
  for(i = 0; i < 3; i++) {
-   a[i] = function(){ return i; } 
+   a[i] = function(){ return i; }       // inside the single scope of f()
  }
  return a;
 }
@@ -64,10 +81,9 @@ console.log(a[0]());  //Output : 3
 console.log(a[1]());  //Output : 3 
 console.log(a[2]());  //Output : 3
 
-/*
-The solution is to use another closure,
-When you pass an argument to a function it becomes available as a local variable
-*/
+
+// The solution is to use another closure, When you pass an argument to a function it becomes available as a local variable
+
 
 function f() {
  var a = [];
@@ -75,12 +91,15 @@ function f() {
  for(i = 0; i < 3; i++) {
  a[i] = (function(x){
              return function(){
-                               return x;           // The scope of inner function is outside function which have local copy of x,  three function have different copy of x with different value.
+                     return x;   // Created three different scopes with three self invoking functions.
                     }
          })(i);
  }
  return a;
 }
+
+
+// The difference is we have created three different scopes in above example rather than single scope. Note: the final value in the scope is remembered only.
 
 /*
 
@@ -93,5 +112,6 @@ function f(x){
                      return a;                           
           }
 }
-
 */
+
+
