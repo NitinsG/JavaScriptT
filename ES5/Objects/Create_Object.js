73263397bec46aa9cba2c227dac9e1a584061eb9
NitinsG <36042347+NitinsG@users.noreply.github.com>
2020-04-03 18:14:36 +0530

    Multi Level Inheritance

--- ES5/Objects/Create_Object.js
+++ ES5/Objects/Create_Object.js
@@ -46,7 +46,33 @@ Object.defineProperty(cat, 'fullName',
     }
   })
 
-cat.fullName = 'Muffin Top'
-display(cat.fullName)
-display(cat.name.first)
-display(cat.name.last)
+cat.fullName = 'Muffin Top'       // set will be called here setting name.first and name.last   
+display(cat.fullName)             // get will call here with output Muffin Top
+display(cat.name.first)           // Output : Muffin  
+display(cat.name.last)            // Output : Top
+
+//Inheritance Multi Level
+
+'use strict';
+
+class Animal {
+  constructor(voice) {
+    this.voice = voice || 'grunt'
+  }
+  
+  speak() {
+    display(this.voice)
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, color) {
+    super('Meow')
+    this.name = name
+    this.color = color
+  }
+}
+
+var fluffy = new Cat('Fluffy', 'White')
+display(Object.keys(fluffy.__proto__.__proto__))               
+display(fluffy.__proto__.__proto__.hasOwnProperty('speak'))
