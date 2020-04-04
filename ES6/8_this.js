7ff37d54e6664fe868f50822de91b024b3c617e4
NitinsG <36042347+NitinsG@users.noreply.github.com>
2020-04-04 22:08:40 +0530

    Update 8_this.js

--- ES6/8_this.js
+++ ES6/8_this.js
@@ -33,7 +33,18 @@ let o = { carId: 123,
                  }
         }
         
-console.log(o.getId()); // 123    Here 'this' refers to o.
+console.log(o.getId()); // 123    Here 'this' refers to o. And carId is the property of object, instead of independent variable.
+// As shown below with example of independent variable
+
+let carId = 234;
+let o = { carId: 123, 
+          getId: function() {
+                    return carId;    // return window.carId , which is 234, this.carId is 123.
+                 }
+        }
+        
+console.log(o.getId());   // 234 , Here 
+
 
 
 
