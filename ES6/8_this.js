d5d70d04d9ee79da1b211e6f0810706f37b25f89
NitinsG <36042347+NitinsG@users.noreply.github.com>
2020-04-08 13:44:08 +0530

    Update 8_this.js

--- ES6/8_this.js
+++ ES6/8_this.js
@@ -98,11 +98,29 @@ var Task = function (name) {
  this.complete = function () {
     console.log('completing task: ' + this.name);
     this.completed = true;
- };
+ }
 
  this.save = function () {
     console.log('saving Task: ' + this.name);
- };
+ }
 };                                   
+
+var task1 = new Task('Task for constructor');
+var task2 = new Task('Task for Singleton');
+                                   
+//Other way
+var Task = function (name) {
+    this.name = name;
+    this.completed = false;
+}
+
+Task.prototype.complete = function () {
+    console.log('completing task: ' + this.name);
+    this.completed = true;
+};
+
+Task.prototype.save = function () {
+    console.log('saving Task: ' + this.name);
+};
 
                                    
