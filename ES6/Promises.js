5194adc46f00e97cf6648df67919501b9dde619a
NitinsG <36042347+NitinsG@users.noreply.github.com>
2020-12-08 23:05:59 +0530

    Update Promises.js

--- ES6/Promises.js
+++ ES6/Promises.js
@@ -15,6 +15,8 @@ Promises solves two purposes:
    
 2. Other aspect is when you dont know when the object or value will be avaiable in future. Then in that case , Promise is th best way to handle that situation.
 
+Note : Promises have 3 states Pending , Fullfilled , Rejected.
+
 */
 
 For Point 1 , below is the example
@@ -80,3 +82,57 @@ export function callbacks() {
 }
 
 //Above is also not the efficent of handling the situation.
+
+//Promises using axiom
+
+export function get(){
+    axios.get("http://localhost:3000/orders/1")
+        .then(({data}) => setText(JSON.stringify(data)));
+}
+
+export function getCatch(){
+    axios.get("http://localhost:3000/orders/123")                                        // first promise object is returned on which tehn function is called
+    .then(({data}) => setText(data.id))                                                  // then function of first promise object getting data.  
+    .catch((err) => setText(err));
+}
+
+export function chain(){
+    axios.get("http://localhost:3000/orders/1")                                            // first promise object is retuned.
+    .then(({data}) => {                                                                    // Here we making an another call so return is reuqired to return the 
+        return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`);       // second promise object instead of first.
+    })
+    .then(({data}) => {
+        setText(`City: ${data.city}`);
+    })
+}
+
+export function chainCatch(){
+    axios.get("http://localhost:3000/orders/1")
+    .then(({data}) => {
+        axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`);
+        throw new Error('Fake Error');
+    })
+    .then(({data}) => {
+        setText(`City: ${data.city}`);
+    })
+    .catch(setText);
+}
+
+// finally is the block that always runs whether the promise is resolved or rejected.
+export function final(){
+    showWaiting();
+    axios.get("http://localhost:3000/orders/1")
+    .then(({data}) => {
+        return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`);
+    })
+    .then(({data}) => {
+        setText(`City: ${data.city}`);
+    })
+    .catch(setText)
+    .finally(() => {
+        setTimeout(() => {
+            hideWaiting();
+        }, 1500);
+        appendText(' -- COMPLETELY DONE');
+    });
+}
